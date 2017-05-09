e6c8c62439cbd1c77a485d39744be1ec376f2bca
Kassia Catarine <kassia_catarine.15@hotmail.com>
2017-05-09 13:55:18 -0300

    Criação do app core

---
+++ core/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+# from django.http import HttpResponse # Envia para o servidor um response
+
+def home(request):
+    # return HttpResponse(u'Olá Mundo') # envia para o servidor 'Ola Mundo'
