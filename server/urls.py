0ca1d79508303acbff5752aa769df037ceb33146
Kassia Catarine <kassia_catarine.15@hotmail.com>
2017-05-10 09:01:09 -0300

    Criando arquivo separado para controle de url dos apps

--- server/urls.py
+++ server/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from core import views # Imposta as funções existentes na view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name='home'),# Chama a função existente na view
-    url(r'^contato/', views.contact, name='contact'),
+    url(r'^', include('pyprende.core.urls', namespace='core')),# Chama o arquivo de urls do app core
 ]
