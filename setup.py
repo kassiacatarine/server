27b4647b232be30a237f52bb0c2f7bc78bc8cc46
Kassia Catarine <kassia_catarine.15@hotmail.com>
2017-05-16 12:50:23 -0300

    Configurando a execução com pytest
    Signed-off-by: Kassia Catarine <kassia_catarine.15@hotmail.com>

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+
+    """Setup the py.test test runner."""
+
+    def finalize_options(self):
+        """Set options for the command line."""
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        """Execute the test runner command."""
+        # Import here, because outside the required eggs aren't loaded yet
+        import pytest
+        sys.exit(pytest.main(self.test_args))
