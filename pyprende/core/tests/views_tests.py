4d0222347b369c9d1692190f66f4d13b85cfd84f
Kassia Catarine <kassia_catarine.15@hotmail.com>
2017-05-16 10:43:38 -0300

    Alterando tests
    Signed-off-by: Kassia Catarine <kassia_catarine.15@hotmail.com>

--- pyprende/core/tests/views_tests.py
+++ pyprende/core/tests/views_tests.py
@@ -5,31 +5,34 @@ from django.core.urlresolvers import reverse
 
 
 class HomeViewTest(TestCase):
-    def setUp(self):
-        client = Client()
-    
     def test_home_status_code(self):
-        response = self.client.get(reverse('core:home'))
+        client = Client()
+        response = client.get(reverse('core:home'))
         self.assertEqual(response.status_code, 200)
 
     def test_home_used_base(self):
-        response = self.client.get(reverse('core:home'))
+        client = Client()
+        response = client.get(reverse('core:home'))
         self.assertTemplateUsed(response, 'home.html')
         self.assertTemplateUsed(response, 'base.html')
 
     def test_home_not_used_contact(self):
-        response = self.client.get(reverse('core:home'))
+        client = Client()
+        response = client.get(reverse('core:home'))
         self.assertTemplateNotUsed(response, 'contact.html')
 
     def test_contact_status_code(self):
-        response = self.client.get(reverse('core:contact'))
+        client = Client()
+        response = client.get(reverse('core:contact'))
         self.assertEqual(response.status_code, 200)
 
     def test_contact_used_base(self):
-        response = self.client.get(reverse('core:contact'))
+        client = Client()
+        response = client.get(reverse('core:contact'))
         self.assertTemplateUsed(response, 'contact.html')
         self.assertTemplateUsed(response, 'base.html')
 
     def test_contact_not_used_home(self):
-        response = self.client.get(reverse('core:contact'))
+        client = Client()
+        response = client.get(reverse('core:contact'))
         self.assertTemplateNotUsed(response, 'home.html')
